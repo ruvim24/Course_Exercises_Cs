08966a8c511f86bdcafdd10fbdf78395e731d039
Stratila Ruvim <ruvim.stratila@gmail.com>
2024-08-02 14:51:38 +0300

    added xTests

---
+++ Course_Exercises_Cs/UTs/StringValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_Exercises_Cs.UTs
+{
+    public static class StringValidator
+    {
+        public static bool IsValidEmail(string email)
+        {
+            return email.Contains("@") && email.Contains(".");
+        }
+
+
+        public static bool IsPhoneNumber(string phoneNumber)
+        {
+            return phoneNumber.Length == 10 && phoneNumber.All(char.IsDigit);
+        }
+
+    }
+}
