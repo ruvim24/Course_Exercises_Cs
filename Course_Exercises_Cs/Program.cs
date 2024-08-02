722c1d390c5a9b4dacdba3f0e34b8785af8ffb1a
Stratila Ruvim <ruvim.stratila@gmail.com>
2024-08-02 14:34:28 +0300

    Add project files.

---
+++ Course_Exercises_Cs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Course_Exercises_Cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
